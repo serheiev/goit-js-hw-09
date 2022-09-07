84439c686a795e8842a15f42ecf21295e8385732
serheiev <69833095+serheiev@users.noreply.github.com>
2022-09-07 17:17:15 +0000

    Deploying to gh-pages from @ serheiev/goit-js-hw-09@79b2f32678057132960ae2c07eb81fbf825a3824 🚀

---
+++ 01-color-switcher.c8bd9fcc.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let d=null;e.setAttribute("disabled",!0);t.addEventListener("click",(function(){d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(d),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}));
+//# sourceMappingURL=01-color-switcher.c8bd9fcc.js.map
